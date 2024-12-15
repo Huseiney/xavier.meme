049b33529fa8a1836bdcc7d825f71deba703c2b5
Huseiney <96485255+Huseiney@users.noreply.github.com>
2024-12-15 08:46:38 +0300

    Update script.js

--- script.js
+++ script.js
@@ -10,20 +10,18 @@ document.addEventListener("DOMContentLoaded", () => {
     }, 1000);
 });
 
-// Animate Logo Colors
-document.addEventListener("DOMContentLoaded", () => {
-    const logo = document.getElementById("animatedLogo");
-    let colors = ["#F472B6", "#3B82F6", "#FBBF24", "#A855F7"];
-    let index = 0;
+// Copy Contract Address
+function copyCA() {
+    const address = document.getElementById("contractAddress").textContent;
+    navigator.clipboard.writeText(address).then(() => {
+        alert("Contract Address Copied!");
+    }).catch(err => {
+        console.error("Failed to copy address: ", err);
+    });
+}
 
-    setInterval(() => {
-        logo.style.borderColor = colors[index];
-        index = (index + 1) % colors.length;
-    }, 1000);
-});
-
-// Copy Wallet Address
-function copyAddress() {
+// Copy SOL Donate Address
+function copySOL() {
     const address = document.getElementById("solAddress").textContent;
     navigator.clipboard.writeText(address).then(() => {
         alert("SOL Wallet Address Copied!");
